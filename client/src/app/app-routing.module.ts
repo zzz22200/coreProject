f0ec2e204ed3b084eba21d297eb29b9811c1e6ab
zzz22200 <zxc321zxc0@gmail.com>
2019-06-21 00:46:35 +0800

    init

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+
+import {PageNotFoundComponent} from './error-routing/not-found/not-found.component';
+import {UncaughtErrorComponent} from './error-routing/error/uncaught-error.component';
+import {ErrorRoutingModule} from './error-routing/error-routing.module';
+import {DoctorInfoComponent} from './doctor-info/doctor-info.component';
+
+export const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent, data: {text: 'Home'}},
+  {path: 'error', component: UncaughtErrorComponent},
+  {path: 'doctorInfo', component: DoctorInfoComponent},
+  {path: '**', component: PageNotFoundComponent} // must always be last
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes), ErrorRoutingModule],
+  exports: [RouterModule, ErrorRoutingModule]
+})
+export class AppRoutingModule {
+}
